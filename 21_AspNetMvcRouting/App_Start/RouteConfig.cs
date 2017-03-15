9dacc704e378f5d2fa894c485c91c8a8130e1d23
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-15 16:17:45 +0300

    Routing yapisini anlama

--- 21_AspNetMvcRouting/App_Start/RouteConfig.cs
+++ 21_AspNetMvcRouting/App_Start/RouteConfig.cs
@@ -11,13 +11,51 @@ namespace _21_AspNetMvcRouting
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");  //.axd uzantilara erisim yasaklaniyor.(guvenlik icin)
+
+            //Mvc Nedir? Nasil Kullanilir?
+            //mvc-nedir-nasil-kullanilir
+            //...makale/goster/mvc-nedir-nasil-kullanilir
+
+            routes.MapRoute(
+           name: "hakkimizda",
+           url: "Hakkimizda",
+           defaults: new { controller = "Makele", action = "Goster", makaleadi = UrlParameter.Optional }
+       );
+
+
+
+
+            routes.MapRoute(
+            name: "hakkimizda",
+            url: "Hakkimizda",
+            defaults: new { controller = "Home", action = "About", id = 1, url = "abc" }
+        );
+            routes.MapRoute(
+            name: "iletisim",
+            url: "Iletisim",
+            defaults: new { controller = "Home", action = "Contact", id = 1, url = "abc" }
+        );
+            routes.MapRoute(
+               name: "anasayfa",
+               url: "Anasayfa",
+               defaults: new { controller = "Home", action = "Index", id=1,url="abc" }
+           );
+
+
+            routes.MapRoute(
+            name: "Default2",
+            url: "{controller}/Belge/{belgeNo}", //Home/Index(int id)
+            defaults: new { controller = "Home", action = "Belge", belgeNo = UrlParameter.Optional }
+        );
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}", //Home/Index(int id)
+                defaults: new { controller = "Home", action = "Index",id=UrlParameter.Optional}
             );
+
+          
         }
     }
 }
